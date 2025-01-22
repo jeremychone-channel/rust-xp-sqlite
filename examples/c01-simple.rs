e01aeb71ec193e9577cb00c6227c260580b6bf64
Jeremy Chone <jeremy.chone@gmail.com>
2025-01-21 17:52:07 -0800

    . c01-simple - simple create table / insert / select

--- examples/c01-simple.rs
+++ examples/c01-simple.rs
@@ -1,5 +1,38 @@
+use rusqlite::Connection;
+use xp_sqlite::db_utils::print_table;
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
 	println!("Hello, world!");
+	// -- Memory SQLite
+	let conn = Connection::open_in_memory()?;
+
+	// -- Create schema
+	conn.execute(
+		"CREATE TABLE person (
+            id    INTEGER PRIMARY KEY,
+            name  TEXT NOT NULL,
+						yob   INTEGER, -- year of birth
+            data  BLOB
+        ) STRICT",
+		(), // empty list of parameters.
+	)?;
+
+	// -- Insert `number` in `string` column
+	// OK in `strict` mode
+	conn.execute("INSERT INTO person (name, yob) VALUES (?1, ?2)", ("Jen", &2000))?;
+
+	// -- Select
+	let mut stmt = conn.prepare("SELECT person.id, person.name, person.yob FROM person WHERE yob > :yob")?;
+	let mut rows = stmt.query(&[(":yob", &1900)])?;
+
+	while let Some(row) = rows.next()? {
+		let name: String = row.get(1)?;
+		println!("->> name: {name}");
+		println!("->>  row: {row:?}");
+	}
+
+	// -- Pretty print table
+	print_table(&conn, "person")?;
 
 	Ok(())
 }
---
+++ src/db_utils/mod.rs
@@ -0,0 +1,7 @@
+// region:    --- Modules
+
+mod db_print;
+
+pub use db_print::*;
+
+// endregion: --- Modules
