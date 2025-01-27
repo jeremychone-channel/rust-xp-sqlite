ca847d8b7c46baaaabbccd914d6811ab9a63f925
Jeremy Chone <jeremy.chone@gmail.com>
2025-01-26 20:07:02 -0800

    * c07-db... - first pass but query

--- examples/c07-db-manager.rs
+++ examples/c07-db-manager.rs
@@ -1,4 +1,5 @@
 use rusqlite::Connection;
+use xp_sqlite::db::Db;
 use xp_sqlite::db_utils::{create_schema, print_table};
 
 const DB_PATH: &str = "_my-db.db3";
@@ -9,17 +10,26 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 	let conn = Connection::open(DB_PATH)?;
 	create_schema(&conn)?;
 
+	let db = Db::new(DB_PATH)?;
+
 	let names = &["Jen", "Mike"];
+	let mut ids: Vec<i64> = Vec::new();
 
 	for name in names {
 		for i in 1..10 {
 			let name = format!("{name}-{i}");
-			let res = tokio::task::spawn(async move {
-				let conn = Connection::open(DB_PATH).map_err(|err| err.to_string())?;
-				conn.execute("INSERT INTO person (name, yob) VALUES (?1, ?2)", (name, &2000))
-					.map_err(|err| err.to_string())
+			let db = db.clone();
+			let id = tokio::task::spawn(async move {
+				// let conn = Connection::open(DB_PATH).map_err(|err| err.to_string())?;
+				db.execute_with_running_i64(
+					"INSERT INTO person (name, yob) VALUES (?1, ?2) RETURNING id",
+					(name, &2000),
+				)
+				.await
+				.map_err(|err| err.to_string())
 			})
 			.await??;
+			ids.push(id);
 		}
 	}
 
--- src/db.rs
+++ src/db.rs
@@ -1,21 +1,22 @@
 use crate::Result;
 use rusqlite::types::Value;
-use rusqlite::Connection;
+use rusqlite::{Connection, Params, Row, Rows, Statement};
 use std::sync::{Arc, Mutex};
 
+#[derive(Clone)]
 pub struct Db {
 	/// If None, then, in memory
 	/// Otherwise path of the file e.g., `./my-db.db3`
-	path: Option<String>,
+	path: Option<Arc<str>>,
 	conn: Arc<Mutex<Connection>>,
 }
 
 /// Constructors
 impl Db {
 	/// Create a new Db instance
-	pub fn new(path: impl Into<String>) -> Result<Self> {
+	pub fn new(path: impl Into<Arc<str>>) -> Result<Self> {
 		let path = path.into();
-		let conn = Connection::open(&path)?;
+		let conn = Connection::open(path.as_ref())?;
 		Ok(Self {
 			path: Some(path),
 			conn: Arc::new(Mutex::new(conn)),
@@ -33,18 +34,29 @@ impl Db {
 
 /// Executors
 impl Db {
-	pub async fn execute(&self, sql: &str) -> Result<usize> {
+	pub async fn execute(&self, sql: &str, params: impl Params) -> Result<usize> {
 		let conn = self.conn.lock().map_err(|err| err.to_string())?;
-		let num = conn.execute(sql, ())?;
+		let num = conn.execute(sql, params)?;
 		Ok(num)
 	}
 
 	/// Assume the sql will end with `RETURNING _some_i64_column_`
-	pub async fn execute_with_running_i64(&self) -> Result<i64> {
-		todo!()
+	pub async fn execute_with_running_i64(&self, sql: &str, params: impl Params) -> Result<i64> {
+		let conn = self.conn.lock().map_err(|err| err.to_string())?;
+		let mut stmt = conn.prepare(sql)?;
+		let org_id = stmt.query_row(params, |r| r.get::<_, i64>(0))?;
+
+		Ok(org_id)
 	}
 
-	pub async fn query(sql: &str, params: ()) -> Result<Vec<Field>> {
+	pub async fn query(&self, sql: &str, params: impl Params) -> Result<Vec<Vec<Field>>> {
+		let conn = self.conn.lock().map_err(|err| err.to_string())?;
+		let mut statement = conn.prepare(sql)?;
+		let rows = statement.query(params)?;
+
+		let mut owned_rows: Vec<Vec<Field>> = Vec::new();
+
+		// Ok(rows)
 		todo!()
 	}
 }
